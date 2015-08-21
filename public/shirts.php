e0dba8bcbbfdbfaf655337a6bb83acf887924089
evgkit <kitaev.ea@gmail.com>
2015-08-21 21:50:01 +0300

    Add products mockup

--- public/shirts.php
+++ public/shirts.php
@@ -1,12 +1,27 @@
 <?
 $pageTitle = 'Mike\'s Full Catalog of Shirts';
 $section = 'shirts';
-include 'include/header.php'; ?>
 
-<div class="section page">
+include 'include/header.php';
+include 'include/products.php';
+?>
 
-    <h1>Mike&rsquo;s Full Catalog of Shirts</h1>
+<div class="section shirts page">
+    <div class="wrapper">
 
+        <h1>Mike&rsquo;s Full Catalog of Shirts</h1>
+
+        <ul class="products">
+            <? foreach($products as $product_id => $product) {
+                ?><li>
+                    <a href="shirt.php?id=<?= $product_id ?>">
+                        <img src="<?= $product["img"] ?>" alt="<?= $product["name"] ?>">
+                        <p>View Details</p></a>
+                </li><?
+            } ?>
+        </ul>
+
+    </div>
 </div>
 
 <? include 'include/footer.php'; ?>
